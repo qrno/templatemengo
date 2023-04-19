54b63d65db1ad7564fe33135b284d806cc8b1852
Eduardo Quirino <eduardo.quirino.oliveira@gmail.com>
2023-04-19 20:38:39 -0300

    Updated template.cpp

--- code/template.cpp
+++ code/template.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Template (v1.3.3 - 2023-02-05) (codeforces:cebolinha, atcoder:edu) {{{
+// Template (v1.4.0 - 2023-04-19) (codeforces:cebolinha, atcoder:edu) {{{
 
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
@@ -17,8 +17,7 @@ template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tr
 template<class T> using V = vector<T>;
 template<class T> using min_priority_queue = priority_queue<T, vector<T>, greater<T>>;
 using ii = pair<int, int>;
-using iii = tuple<int, int, int>;
-using ll = long long;
+using iii = array<int, 3>;
 
 #define all(c) c.begin(), c.end()
 #define rall(c) c.rbegin(), c.rend()
@@ -35,18 +34,18 @@ using ll = long long;
 #define L1(res...) [&](auto x){ return res; }
 #define L2(res...) [&](auto x, auto y){ return res; }
 
-template<class T, class U> inline void miq(T& a, U b){if (a > b) a = b;}
-template<class T, class U> inline void maq(T& a, U b){if (a < b) a = b;}
+template<class T, class U> inline void miq(T& a, U b){ if (a > b) a = b; }
+template<class T, class U> inline void maq(T& a, U b){ if (a < b) a = b; }
 
-template<class T, class U> istream &operator>>(istream &is, pair<T, U> &p) { is >> p.ff >> p.ss; return is; }
-template<class T> istream &operator>>(istream &is, vector<T> &v) { for (auto &a : v) is >> a; return is; }
-template<class T, class U> ostream &operator<<(ostream &os, pair<T, U> const& p) { os << "(" << p.first << " " << p.second << ")"; return os; }
-template<class T> ostream &operator<<(ostream &os, vector<T> const& v) { for (int i = 0; i < v.size(); i++) os << cond(i," ","") << v[i]; return os; }
-template<class T, class U> ostream &operator<<(ostream &os, map<T, U> const& m) { bool first = true; for (auto const& [k, v] : m) { if (!first) os << " "; first = false; os << "{" << k << " : " << v << "}"; } return os; }
-template<class T> ostream &operator<<(ostream &os, set<T> const& s) { for (auto it = s.begin(); it != s.end(); it++) os << cond(it != s.begin()," ","") << *it; return os; }
+template<class T, class U> auto &operator>>(istream &is, pair<T, U> &p) { return is >> p.ff >> p.ss; }
+template<class T, class U> auto &operator<<(ostream &os, pair<T, U> const& p) { return os << '(' << p.first << ' ' << p.second << ')'; }
+
+const auto EMPTY_STRING = "", SEPARATOR = " ";
+template<class C, typename T = typename C::value_type> auto &operator>>(istream &is, C &c) { for (auto &x : c) is >> x; return is; }
+template<class C, typename T = typename C::value_type> auto &operator<<(ostream& os, C const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
 
 template<class... A> void in(A &...a) { ((cin >> a), ...); }
-template<class... A> void out(A const&... a) { ((cout << a << " "), ...); cout << endl; }
+template<class... A> void out(A const&... a) { auto sep = EMPTY_STRING; ((cout << sep << a, sep = SEPARATOR), ...); cout << '\n'; }
 template<class... A> void print(A const&... a) { ((cout << a), ...); }
 #define var(x) "[", #x, " ", x, "] "
 template<class... A> void db(A const&... a) { ((cout << (a)), ...); cout << endl; }
