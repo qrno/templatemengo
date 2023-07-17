0ebb30cfff108cf0f48e8bcd3fe2b8415ad420da
Eduardo Quirino <eduardo.quirino.oliveira@gmail.com>
2023-07-17 00:49:37 -0300

    changed pragmas, slightly changed IO

--- code/template.cpp
+++ code/template.cpp
@@ -1,24 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Template (v1.4.3 - 2023-04-22) (codeforces:cebolinha, atcoder:edu) {{{
-
+// Template (v1.5.0 - 2023-07-17) (codeforces:cebolinha, atcoder:edu) {{{
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
 
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 
 #define int long long
 #define fastio ios::sync_with_stdio(false); cin.tie(nullptr)
 
-template<class T> using V = vector<T>;
 template<class T> using min_priority_queue = priority_queue<T, vector<T>, greater<T>>;
 using ii = pair<int, int>;
 using iii = array<int, 3>;
 
+#define V vector
 #define all(c) c.begin(), c.end()
 #define rall(c) c.rbegin(), c.rend()
 #define sz(x) (int) (x).size()
@@ -40,19 +39,19 @@ template<class T, class U> inline void maq(T& a, U b){ if (a < b) a = b; }
 template<class T, class U> auto &operator>>(istream &is, pair<T, U> &p) { return is >> p.ff >> p.ss; }
 template<class T, class U> auto &operator<<(ostream &os, pair<T, U> const& p) { return os << '(' << p.first << ' ' << p.second << ')'; }
 
-const auto EMPTY_STRING = "", SEPARATOR = " ";
+const auto ES = "", SEP = " ";
 template<class T> auto &operator>>(istream& is, vector<T> &c) { for (auto &x : c) is >> x; return is; }
-template<class T> auto &operator<<(ostream& os, vector<T> const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
-template<class T> auto &operator<<(ostream& os, set<T> const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
-template<class T> auto &operator<<(ostream& os, multiset<T> const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
-template<class T> auto &operator<<(ostream& os, unordered_set<T> const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
-template<class T> auto &operator<<(ostream& os, ordered_set<T> const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
-template<class T> auto &operator<<(ostream& os, deque<T> const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
-template<class K, class V> auto &operator<<(ostream& os, map<K,V> const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
-template<class K, class V> auto &operator<<(ostream& os, unordered_map<K,V> const &c) { auto sep = EMPTY_STRING; for (auto x : c) os << sep << x, sep = SEPARATOR; return os; }
+template<class T> auto &operator<<(ostream& os, vector<T> const &c) { auto sep = ES; for (auto x : c) os << sep << x, sep = SEP; return os; }
+template<class T> auto &operator<<(ostream& os, set<T> const &c) { auto sep = ES; for (auto x : c) os << sep << x, sep = SEP; return os; }
+template<class T> auto &operator<<(ostream& os, multiset<T> const &c) { auto sep = ES; for (auto x : c) os << sep << x, sep = SEP; return os; }
+template<class T> auto &operator<<(ostream& os, unordered_set<T> const &c) { auto sep = ES; for (auto x : c) os << sep << x, sep = SEP; return os; }
+template<class T> auto &operator<<(ostream& os, ordered_set<T> const &c) { auto sep = ES; for (auto x : c) os << sep << x, sep = SEP; return os; }
+template<class T> auto &operator<<(ostream& os, deque<T> const &c) { auto sep = ES; for (auto x : c) os << sep << x, sep = SEP; return os; }
+template<class K, class V> auto &operator<<(ostream& os, map<K,V> const &c) { auto sep = ES; for (auto x : c) os << sep << x, sep = SEP; return os; }
+template<class K, class V> auto &operator<<(ostream& os, unordered_map<K,V> const &c) { auto sep = ES; for (auto x : c) os << sep << x, sep = SEP; return os; }
 
 template<class... A> void in(A &...a) { ((cin >> a), ...); }
-template<class... A> void out(A const&... a) { auto sep = EMPTY_STRING; ((cout << sep << a, sep = SEPARATOR), ...); cout << '\n'; }
+template<class... A> void out(A const&... a) { auto sep = ES; ((cout << sep << a, sep = SEP), ...); cout << '\n'; }
 template<class... A> void print(A const&... a) { ((cout << a), ...); }
 #define var(x) "[", #x, " ", x, "] "
 template<class... A> void db(A const&... a) { ((cout << (a)), ...); cout << endl; }
@@ -61,4 +60,6 @@ template<class... A> void db(A const&... a) { ((cout << (a)), ...); cout << endl
 auto main() -> signed {
   fastio;
 
+
 }
+
