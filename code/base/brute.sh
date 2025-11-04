87ba8f625d29e687b2f3c30b111e5fdea3e54c26
Eduardo Quirino <eduardo.quirino.oliveira@gmail.com>
2025-11-04 18:24:53 -0300

    Add a brute.sh

---
+++ code/base/brute.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu
+
+gen="$1"
+p1="$2"
+p2="$3"
+
+for ((i=1; ; i++)) do
+  echo -ne "Test $i\r"
+
+  $gen > i.txt
+  $p1 < i.txt > o1.txt
+  $p2 < i.txt > o2.txt
+  diff -q o1.txt o2.txt
+done
